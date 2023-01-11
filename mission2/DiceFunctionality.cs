3b7659fda498fb09f41eca82da1d1b9921b8e111
Andy Graviet <graviet.andrew@gmail.com>
2023-01-11 13:11:15 -0700

    first commit

---
+++ mission2/DiceFunctionality.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace mission2
+{
+	public class DiceFunctionality
+	{
+		public void Roll(int maxRolls)
+		{
+            Random rnd = new Random();
+            int[] totals = new int[11];
+
+            //LOOP FOR GENERATING ALL THE ROLE VALUES
+            for (int i = 0; i < maxRolls; i++)
+            {
+                //role 2 dice generating random numbers less than or equal to 6
+                int currRoll = rnd.Next(1, 7);
+                currRoll += rnd.Next(1, 7);
+                totals[currRoll - 2] += 1;
+                currRoll = 0;
+                //add them, add 1 to index where index = sum of dice
+            }
+
+            for (int i = 0; i < totals.Length; i++)
+            {
+                //get percentage 
+                float numStars = (totals[i] / (float)maxRolls) * 100;
+                string numStarsString = "";
+                //convert percentage to string of stars
+                for (int j = 0; j < numStars; j++)
+                {
+                    numStarsString += "*";
+                }
+
+                //print the stars
+                Console.WriteLine((i + 2) + ": " + numStarsString + " " + numStars);
+            }
+        }
+	}
+}
+
---
+++ mission2/Program.cs
@@ -0,0 +1,20 @@
+﻿// ANDREW GRAVIET, SECTION 004
+// DICE ROLL GAME MISSION #2
+using System;
+using System.Reflection;
+using static mission2.DiceFunctionality;
+
+namespace mission2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to the dice roller game. \n Please enter how many times the pair of dice will be rolled:");
+            int maxRolls = Int32.Parse(Console.ReadLine()!);
+            DiceFunctionality df = new DiceFunctionality();
+            df.Roll(maxRolls);
+        }
+    }
+}
+
